fe2a228ea73d24411e86253e1250e6b2006ffde9
GeronimoOM <olehmatsuk@gmail.com>
2023-08-07 21:50:43 +0300

    Fix prod API IP

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from '@merged/solid-apollo';
 import { TypePolicy } from '@apollo/client';
 import { LanguageWordsArgs, WordPage } from './types/graphql';
 
-export const GRAPHQL_URI = `${import.meta.env.VITE_API_URL || 'http://localhost:9000'}/graphql`;
+export const API_HOST = import.meta.env.VITE_API_URL || 'http://localhost:9000';
+export const GRAPHQL_URI = `${API_HOST}/graphql`;
 
 const languageTypePolicy: TypePolicy = {
   fields: {
