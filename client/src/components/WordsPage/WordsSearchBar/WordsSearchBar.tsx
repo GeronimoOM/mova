852e4e9c1e5f4701180912b33c040e12e9d4f03d
GeronimoOM <olehmatsuk@gmail.com>
2023-12-21 21:38:12 +0200

    Remove hotkeys from UI

--- client/src/components/WordsPage/WordsSearchBar/WordsSearchBar.tsx
+++ client/src/components/WordsPage/WordsSearchBar/WordsSearchBar.tsx
@@ -1,7 +1,7 @@
 import { Component, createSignal, Show } from 'solid-js';
 import { FaSolidMagnifyingGlass } from 'solid-icons/fa';
 import { WordsSearchParams } from './wordsSearchParams';
-import { WordSearchBarFilterPills } from './WordSearchBarFilterPills';
+//import { WordSearchBarFilterPills } from './WordSearchBarFilterPills';
 import { WordSearchBarDropdown } from './WordSearchBarDropdown';
 import { PartOfSpeech } from '../../../api/types/graphql';
 import { Icon } from '../../common/Icon';
@@ -13,23 +13,6 @@ export enum DropdownMode {
   Topics = 'topics',
 }
 
-//const KEY_FOCUS_SEARCHBAR = 's';
-
-const KEY_DROPDOWN_FILTERS = 'Semicolon';
-const KEY_BACK = 'Backspace';
-const KEY_TO_DROPDOWN_MODE: Record<string, DropdownMode> = {
-  KeyP: DropdownMode.PartsOfSpeech,
-  KeyT: DropdownMode.Topics,
-};
-const KEY_TO_PART_OF_SPEECH: Record<string, PartOfSpeech> = {
-  KeyN: PartOfSpeech.Noun,
-  KeyV: PartOfSpeech.Verb,
-  KeyJ: PartOfSpeech.Adj,
-  KeyD: PartOfSpeech.Adv,
-  KeyP: PartOfSpeech.Pron,
-  KeyM: PartOfSpeech.Misc,
-};
-
 export type WordsSearchBarProps = {
   searchParams: WordsSearchParams;
   onSearchParamsChange: (searchParams: Partial<WordsSearchParams>) => void;
@@ -39,20 +22,6 @@ export const WordsSearchBar: Component<WordsSearchBarProps> = (props) => {
   const [dropdownMode, setDropdownMode] = createSignal<DropdownMode | null>(
     null,
   );
-  const [inputElement, setInputElement] = createSignal<
-    HTMLInputElement | undefined
-  >();
-
-  // const keyEvent = useKeyDownEvent();
-
-  // TODO extract keyboard logic
-  // createEffect(() => {
-  //   const event = keyEvent();
-  //   if (event?.key === KEY_FOCUS_SEARCHBAR) {
-  //     event.preventDefault();
-  //     inputElement()!.focus();
-  //   }
-  // });
 
   const onPartOfSpeechSelect = (pos: PartOfSpeech) => {
     if (props.searchParams.partsOfSpeech.includes(pos)) {
@@ -72,71 +41,6 @@ export const WordsSearchBar: Component<WordsSearchBarProps> = (props) => {
     props.onSearchParamsChange({ query: text });
   };
 
-  const onKeyDown = (code: string): boolean => {
-    if (code === KEY_DROPDOWN_FILTERS) {
-      setDropdownMode(DropdownMode.Filters);
-
-      return false;
-    } else {
-      switch (dropdownMode()) {
-        case null:
-          return onDefaultKeyDown(code);
-        case DropdownMode.Filters:
-          return onFiltersKeyDown(code);
-        case DropdownMode.PartsOfSpeech:
-          return onPartsOfSpeechKeyDown(code);
-      }
-    }
-
-    return true;
-  };
-
-  const onDefaultKeyDown = (code: string): boolean => {
-    if (code === KEY_BACK && inputElement()?.selectionStart === 0) {
-      if (props.searchParams.topics.length) {
-        props.onSearchParamsChange({ topics: [] });
-      } else if (props.searchParams.partsOfSpeech.length) {
-        props.onSearchParamsChange({ partsOfSpeech: [] });
-      }
-      return false;
-    }
-
-    return true;
-  };
-
-  const onFiltersKeyDown = (code: string): boolean => {
-    const dropdownModeByKey = KEY_TO_DROPDOWN_MODE[code];
-    if (dropdownModeByKey) {
-      setDropdownMode(dropdownModeByKey);
-      return false;
-    } else if (code === KEY_BACK) {
-      setDropdownMode(null);
-      return false;
-    }
-
-    return true;
-  };
-
-  const onPartsOfSpeechKeyDown = (code: string): boolean => {
-    const partOfSpeechByKey = KEY_TO_PART_OF_SPEECH[code];
-    if (partOfSpeechByKey) {
-      onPartOfSpeechSelect(partOfSpeechByKey);
-      return false;
-    } else if (code === KEY_BACK) {
-      setDropdownMode(DropdownMode.Filters);
-    }
-
-    return true;
-  };
-
-  const onFilterSelect = (mode: DropdownMode) => {
-    if (mode === dropdownMode()) {
-      setDropdownMode(null);
-    } else {
-      setDropdownMode(mode);
-    }
-  };
-
   const { base: baseColors } = useColorContext()!;
 
   return (
@@ -145,28 +49,28 @@ export const WordsSearchBar: Component<WordsSearchBarProps> = (props) => {
         class={`relative flex w-full flex-row items-center ${baseColors?.textColor} ${baseColors?.backgroundColor}`}
       >
         <Icon icon={FaSolidMagnifyingGlass} />
-        <WordSearchBarFilterPills
+        {/* <WordSearchBarFilterPills
           dropdownMode={dropdownMode()}
           searchParams={props.searchParams}
           onFilterSelect={onFilterSelect}
-        />
+        /> */}
         <input
           class="relative w-full bg-inherit p-3 outline-none placeholder:text-opacity-50"
           type="text"
-          ref={setInputElement}
+          //ref={setInputElement}
           spellcheck={false}
           autoCapitalize="off"
           placeholder={'Search'}
           value={props.searchParams.query}
           onInput={(e) => onInputChange(e.currentTarget.value)}
           onChange={(e) => onInputChange(e.currentTarget.value)}
-          onKeyDown={(e) => {
-            const shouldChangeInput = onKeyDown(e.code);
-            if (!shouldChangeInput) {
-              e.preventDefault();
-            }
-            e.stopPropagation();
-          }}
+          // onKeyDown={(e) => {
+          //   const shouldChangeInput = onKeyDown(e.code);
+          //   if (!shouldChangeInput) {
+          //     e.preventDefault();
+          //   }
+          //   e.stopPropagation();
+          // }}
         />
         <Show when={dropdownMode()}>
           <WordSearchBarDropdown
